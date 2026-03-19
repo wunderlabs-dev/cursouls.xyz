8910e905a3860b5a07316b164ab6151437fedd74
Balaj Marius <balajmarius@gmail.com>
2026-03-19 13:56:29 +0200

    Format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,17 @@ export const metadata: Metadata = {
     url: BASE_URL,
     title: copy.title,
     description: copy.description,
-    images: [{ url: "/images/og.png", width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT }],
+    images: [
+      { url: "/images/og.png", width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: copy.title,
     description: copy.description,
-    images: [{ url: "/images/og.png", width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT }],
+    images: [
+      { url: "/images/og.png", width: OG_IMAGE_WIDTH, height: OG_IMAGE_HEIGHT },
+    ],
   },
 };
 
